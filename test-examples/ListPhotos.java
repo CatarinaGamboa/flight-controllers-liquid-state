be6b5b822b01ab8e3dfa303645894f2cb6a0b3a0
Simon Chu <cchu2@andrew.cmu.edu>
2023-04-26 11:13:49 -0400

    added listPhotos example

---
+++ test-examples/ListPhotos.java
@@ -0,0 +1,27 @@
+public package io.mavsdk.example;
+
+import io.mavsdk.System;
+import io.mavsdk.camera.Camera;
+import io.reactivex.Completable;
+import io.reactivex.Single;
+
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ListPhotos {
+  private static final Logger logger = LoggerFactory.getLogger(ListPhotos.class);
+
+  public static void main(String[] args) {
+    logger.debug("Starting example: taking and listing photos...");
+
+    System drone = new System();
+    CountDownLatch latch = new CountDownLatch(1);
+
+    // Completable trying to take 50 pictures (stopping in case of error)
+    drone.getCamera().takePhoto();
+    drone.getCamera().listPhotos(Camera.PhotosRange.ALL);
+}
+}
