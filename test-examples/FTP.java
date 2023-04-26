631d6d9f62ebd681f7e41d7c490858c5ad5d3c72
Simon Chu <cchu2@andrew.cmu.edu>
2023-04-26 16:25:47 -0400

    added initialize() call for FTP

--- test-examples/FTP.java
+++ test-examples/FTP.java
@@ -5,7 +5,7 @@ import io.mavsdk.ftp.Ftp;
 public class FTP {
   public static void main(String[] args) {
     Ftp ftp = new Ftp();
-    
+    ftp.initialize();
     // suppose we have a /home directory in the companion computer
     ftp.setRootDirectory("/home");
 
