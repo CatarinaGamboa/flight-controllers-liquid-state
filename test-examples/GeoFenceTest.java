d605142b9402954da4a72274ee472934623be435
Simon Chu <cchu2@andrew.cmu.edu>
2023-04-26 22:11:44 -0400

    added geofence

--- test-examples/GeoFenceTest.java
+++ test-examples/GeoFenceTest.java
@@ -1,3 +1,14 @@
+import io.mavsdk.geofence.Geofence;
+import io.mavsdk.geofence.GeofencePolygon;
+import io.mavsdk.geofence.Geofence.Polygon.FenceType;
 public class GeoFenceTest {
-  
+  public static void main(String[] args) {
+    Geofence geofence = new Geofence();
+    geofence.initialize();
+
+    GeofencePolygon pol
+        = new GeofencePolygon(new double[] {47.39776569, 8.54553292, 0.0, 0.0, 0.0, 0.0}, FenceType.INCLUSION);
+
+    geofence.uploadGeofence(pol);
+  }
 }
